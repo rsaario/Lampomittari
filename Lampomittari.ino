dd79e8cb4993ec128161993bfcea00ebaf4d7045
rsaario <66988997+rsaario@users.noreply.github.com>
2021-01-11 10:48:18 +0200

    toiminto sensorin lukemiselle luotu

--- Lampomittari.ino
+++ Lampomittari.ino
@@ -13,5 +13,8 @@ Serial.println("Temp Display");
 }
 
 void loop(){
-
+  if(T3.timeIsUp()){
+    int reading = analogRead(A0);
+      float voltage = reading * (5000 / 1024.0);
+      float temperature = (voltage - 500) / 10;
 }
