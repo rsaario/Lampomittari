00f25ec55769c7d7a23954593a82c02d9c5772db
rsaario <66988997+rsaario@users.noreply.github.com>
2021-01-18 12:59:28 +0200

    kommenttien lisäys

--- Lampomittari.ino
+++ Lampomittari.ino
@@ -2,10 +2,12 @@
 #include <Smoothed.h>
 #include "ajastin.h"
 
-ajastin T3(1000); 
-
+//määrittää ajastimen nimen ja ajan
+ajastin T1(1000); 
+//määritetään lcd sana ohjaamaan näyttöä                                
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
-
+                                //RS,E,D4,D5,D6,D7
+//määriteään float datatyypeiksi                        
 Smoothed <float> tempAveg;
 float smoothedTempAvg;
 float lastSmoothedTempAvg;
@@ -13,35 +15,44 @@ float lastSmoothedTempAvg;
 void setup()
 {
 Serial.begin(9600);
+//käynnistää näytön
 lcd.begin(16, 2);
 Serial.println("Temp Display");
+//aloitetaan ja määritetään keskiarvo laskennalle arvojen määrä
 tempAveg.begin(SMOOTHED_AVERAGE, 100);
 }
-
+//lämpötilan luku toiminto
 void getTemp(){
   int reading = analogRead(A0);
       float voltage = reading * (5000 / 1024.0);
       float temperature = (voltage - 500) / 10;
+      //lisätään arvo keskiarvo laskentaan
       tempAveg.add(temperature);
+      //haetaan keskiarvo
       smoothedTempAvg = tempAveg.get();     
 }   
-
-void writeLcd(){   
+//näytölle kirjoitus toiminto
+void writeLcd(){ 
+  //tulostaa jos lämpötila ylittää 24 astetta  
    if(smoothedTempAvg >24) {
         lcd.setCursor(0,0);
         lcd.print("Liian l\xe1mmin");
         lcd.setCursor(0,1);
+        //määrittää mitä tulostuu ja muuttaa luvun yhdelle desimaalille
         lcd.print(smoothedTempAvg,1);
+        //tulostaa celsius merkin
         lcd.print("\337C");
         Serial.println(smoothedTempAvg);
-   }else if(smoothedTempAvg <20){
+   }//tulostaa jos lämpötila alittaa 20 astetta
+   else if(smoothedTempAvg <20){
         lcd.setCursor(0,0);
         lcd.print("Liian viile\xe1");
         lcd.setCursor(0,1);
         lcd.print(smoothedTempAvg,1);
         lcd.print("\337C");
         Serial.println(smoothedTempAvg);        
-   }else{
+   }//annettujen arvojen välissä tulostuva ilmoitus
+   else{
         lcd.setCursor(0,0);
         lcd.print("Sopiva ilma ");    
         lcd.setCursor(0,1);
@@ -53,6 +64,8 @@ void writeLcd(){
 
 void updateLcd(){
   lastSmoothedTempAvg = tempAveg.getLast();
+  /*vertaa aikaisempaa keskiarvoa uuteen keskiarvoon ja suorittaa sen 
+  mukaan näytön kirjoitus funktion*/
   if(smoothedTempAvg > lastSmoothedTempAvg || smoothedTempAvg < lastSmoothedTempAvg){
     writeLcd();
   }
@@ -60,7 +73,8 @@ void updateLcd(){
 
 void loop(){
   getTemp();
-  if(T3.timeIsUp()){
+  //kun aika on täynnä suorittaa sisällytetyt funktiot
+  if(T1.timeIsUp()){
    updateLcd();
    tempAveg.clear();
   }  
